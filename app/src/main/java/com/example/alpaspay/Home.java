31c1f4d8ed0b86315d1b77b52b8d17b0ea172bc2
dandalandannn <danielgwen.domingo01@gmail.com>
2025-04-19 14:36:47 +0800

    ADD home AND button intents

--- app/src/main/java/com/example/alpaspay/Home.java
+++ app/src/main/java/com/example/alpaspay/Home.java
@@ -1,6 +1,11 @@
 package com.example.alpaspay;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+
+    private TextView balanceTextView;
+    private TextView btnCashIn;
+    private LinearLayout btnTransactions;
+    private LinearLayout btnWithdraw;
+    private LinearLayout btnAccountSettings;
+    private LinearLayout btnHelpdesk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,62 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        balanceTextView = findViewById(R.id.balance);
+        btnCashIn = findViewById(R.id.btn_cashin);
+        btnTransactions = findViewById(R.id.btn_transactions);
+        btnWithdraw = findViewById(R.id.btn_withraw);
+        btnAccountSettings = findViewById(R.id.btn_account_settings);
+        btnHelpdesk = findViewById(R.id.btn_helpdesk);
+
+        btnCashIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleCashIn();
+            }
+        });
+
+        btnTransactions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleTransaction();
+            }
+        });
+        btnWithdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleWithraw();
+            }
+        });
+        btnAccountSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleAccSettings();
+            }
+        });
+        btnHelpdesk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleHelpDesk();
+            }
+        });
+    }
+
+    private void handleCashIn() {
+        startActivity(new Intent(Home.this, CashIn.class));
+    }
+    private void handleTransaction() {
+        startActivity(new Intent(Home.this, TransactionHistory.class));
+    }
+    private void handleWithraw() {
+        startActivity(new Intent(Home.this, WithrawActivity.class));
+    }
+
+    private void handleAccSettings() {
+        startActivity(new Intent(Home.this, AccountSettings.class));
+    }
+
+    private void handleHelpDesk() {
+        startActivity(new Intent(Home.this, HelpDesk.class));
     }
 }
